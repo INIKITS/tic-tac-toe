6e5c0c51a154964a1e07dad4fab65c4c2e7ec5b7
Dakota Solis <dakota.h.solis@gmail.com>
2021-08-09 21:14:55 -0500

    Setup a player object and added functionality to place a player marker on the gameboard where it is clicked. Next is figuring out how to alternate players.

--- tic.js
+++ tic.js
@@ -1,8 +1,55 @@
-var gameboard = (() => {
-    var gameboard = ['x','x','x','o','o','o','x','x','x'];
-    
-    function generateBoard(){
 
+var createGameboard = (() => {
+    var playerIcon = ['','','','','','','','',''];
+    var gameBoard = document.getElementsByClassName('gameboard');
+
+    function generateBoard(){
+        for (var i = 0; i < gameBoard.length; i++){
+            if (!playerIcon[i]){
+                gameBoard[i].innerHTML = '';
+            } 
+            else{
+                gameBoard[i].innerHTML = playerIcon[i];
+            }
+        }
     }
+    function addMove(Player,steps) {
+        // debugger;
+        playerIcon.splice(steps-1, 1, Player);
+        generateBoard();
+    }
+    return {
+        generateBoard : generateBoard,
+        addMove: addMove
+    };
 
 })();
+
+const Player = (name) => {
+    const getName = () => name;
+    const move = (steps) => createGameboard.addMove(name, steps);
+    return {getName, move};
+}
+
+
+
+
+var displayController = (() => {
+    document.addEventListener('click', function(e){
+        var gridItem = document.querySelectorAll('[id^="grid"]');
+        console.log(gridItem);
+        console.log(e.target.id);
+        for (i=0; i<gridItem.length; i++){
+            var steps = gridItem[i].id.substr(5);
+            if (e.target.id == gridItem[i].id){
+                // debugger;
+                console.log(e.target);
+                user.move(steps);
+            }
+        }
+    })
+})();
+
+createGameboard.generateBoard();
+const user = Player('x');
+const user2 = Player('o');
