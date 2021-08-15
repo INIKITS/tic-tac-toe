18dbb7d55d66ea6781de6f3af8fe98ad8a07e781
Dakota Solis <dakota.h.solis@gmail.com>
2021-08-14 20:32:38 -0500

    Very close to figuring out how to determine a win, but saving progress for now.

--- tic.js
+++ tic.js
@@ -13,7 +13,8 @@ var createGameboard = (() => {
             }
         }
     }
-    function addMove(Player,steps) {
+
+    function addMarker(Player,steps) {
         // if marker exists, do nothing //
         if (playerIcon[steps] != ''){
             return null;
@@ -22,28 +23,30 @@ var createGameboard = (() => {
         playerIcon.splice(steps, 1, Player);
         generateBoard();
         user.turn = !user.turn;
+        displayController.checkForWin();
         }
     }
     return {
+        playerIcon:playerIcon,
         generateBoard : generateBoard,
-        addMove: addMove
+        addMarker: addMarker
     };
 })();
 
 const Player = (name) => {
     const getName = () => name;
     const turn = () => true;
-    const move = (steps) => createGameboard.addMove(name, steps);
+    const move = (steps) => createGameboard.addMarker(name, steps);
     return {getName, move, turn};
 }
 
 var displayController = (() => {
-    document.addEventListener('click', function(e){
+    var gridItem = document.querySelectorAll('[id^="grid"]');
 
+    document.addEventListener('click', function(e){
         // getting grid element from target click // 
-        var gridItem = document.querySelectorAll('[id^="grid"]');
         for (i=0; i<gridItem.length; i++){
-            // retrieving amount of steps for placing player marker in next function //
+            // retrieving amount of steps for placing player marker in addMarker function //
             var steps = gridItem[i].id.substr(5) - 1;
             if (e.target.id == gridItem[i].id){
                 if (user.turn){
@@ -55,6 +58,63 @@ var displayController = (() => {
             }
         }
     })
+    function win(){
+        console.log("Win!")
+    }
+
+    function checkForWin() {
+
+        var winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+        for (var i=0; i<winConditions.length; i++){
+
+            for (var j=0; j<winConditions[i].length-1; j++){
+                var markerIndex = winConditions[i][j];
+                var checkSquare = createGameboard.playerIcon[markerIndex];
+                if (checkSquare === ''){
+                    continue;
+                }
+                else if (checkSquare == createGameboard.playerIcon[markerIndex+1] &&
+                    createGameboard.playerIcon[markerIndex+1] == createGameboard.playerIcon[markerIndex-1]){
+                    win();
+                }
+            }
+        }
+        // if (gridItem[0].innerHTML == gridItem[1].innerHTML && gridItem[1].innerHTML == gridItem[2].innerHTML){
+        //     win();
+        // }
+        // else if (gridItem[3].innerHTML == gridItem[4].innerHTML && gridItem[4].innerHTML == gridItem[5].innerHTML){
+        //     win();
+        // }
+        // else if (gridItem[6].innerHTML == gridItem[7].innerHTML && gridItem[7].innerHTML == gridItem[8].innerHTML){
+        //     win();
+        // }
+        // else if (gridItem[0].innerHTML == gridItem[3].innerHTML && gridItem[6].innerHTML == gridItem[6].innerHTML){
+        //     win();
+        // }
+        // else if (gridItem[1].innerHTML == gridItem[4].innerHTML && gridItem[4].innerHTML == gridItem[7].innerHTML){
+        //     win();
+        // }
+        // else if (gridItem[2].innerHTML == gridItem[5].innerHTML && gridItem[5].innerHTML == gridItem[8].innerHTML){
+        //     win();
+        // }
+        // else if (gridItem[0].innerHTML == gridItem[4].innerHTML && gridItem[4].innerHTML == gridItem[8].innerHTML){
+        //     win();
+        // }
+        // else if (gridItem[2].innerHTML == gridItem[4].innerHTML && gridItem[4].innerHTML == gridItem[6].innerHTML){
+        //     win();
+        // }
+
+    }
+    return {checkForWin : checkForWin};
 })();
 
 createGameboard.generateBoard();
